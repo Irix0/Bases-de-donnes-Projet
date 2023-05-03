c360d74a460168748fad89f234be083aa9894124
Irix0 <33395798+Irix0@users.noreply.github.com>
2023-05-03 10:35:16 +0200

    ajout de lieux

--- www/dashboard/locations.php
+++ www/dashboard/locations.php
@@ -7,7 +7,6 @@ $resultsPerPage = 10;
 ?>
 
 <body>
-
    <?php
    if(!isset($_SESSION['login'])) {
       echo "<script type='text/javascript'>document.location.replace('/login.php');</script>";
@@ -16,9 +15,30 @@ $resultsPerPage = 10;
    }
 
    $bdd = new PDO('mysql:host=ms8db;dbname=groupXX', 'groupXX', 'secret');
+
+   if (isset($_POST['street'])) {
+      if(empty($_POST['id'])){
+         $_POST['id'] = NULL;
+      }
+   
+      if(empty($_POST['comment'])){
+         $_POST['comment'] = NULL;
+      }
+
+      $sql = 'INSERT INTO `location` (`ID`, `STREET`, `CITY`, `POSTAL_CODE`, `COUNTRY`, `COMMENT`) VALUES (:id, :street, :city, :postal, :country, :comment);';
+      $sth = $bdd->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+      $sth->execute(array('id' => NULL, 'street' => $_POST["street"], 'city' => $_POST["city"], 'postal' => $_POST["zip"], 'country' => $_POST["country"], 'comment' => $_POST["comment"]));
+   }
 ?>
 
-   <div class="table-auto overflow-x-auto shadow-md sm:rounded-lg mt-10 ml-80 mr-80">
+   <!-- Add location modal toggle -->
+   <button data-modal-target="add-location-modal" data-modal-toggle="add-location-modal"
+      class="ml-80 mr-80 mt-10 block bg-blue-50 rounded text-sm font-medium text-blue-500 hover:bg-blue-100 hover:text-blue-600 px-5 py-2.5 text-center"
+      type="button">
+      Ajouter un lieu
+   </button>
+   <!-- Main table -->
+   <div class="table-auto overflow-x-auto shadow-md sm:rounded-lg mt-6 ml-80 mr-80">
       <table class="w-full text-sm text-left text-gray-500">
          <thead class="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
@@ -75,7 +95,7 @@ $resultsPerPage = 10;
                   </td>
                   <td class='px-6 py-4'>
                      <a href='/edit-location.php?id=".$row['ID']."'>
-                        <button type='button' class='text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800'>Editer</button>
+                        <button type='button' class='text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'>Editer</button>
                      </a>
                   </td>
                </tr>
@@ -95,7 +115,8 @@ $resultsPerPage = 10;
                   echo $rows_nb;
                } else {
                   echo ($pageFirstResult + $resultsPerPage);
-               } ?></span> entitées montrées sur <span class="font-semibold text-gray-900"><?php echo "$rows_nb"; ?></span>
+               } ?></span> entitées montrées sur <span
+               class="font-semibold text-gray-900"><?php echo "$rows_nb"; ?></span>
          </span>
          <!-- Buttons -->
 
@@ -124,4 +145,75 @@ $resultsPerPage = 10;
 
       </div>
 
+
+      <!-- Main modal -->
+      <div id="add-location-modal" tabindex="-1" aria-hidden="true"
+         class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+         <div class="relative w-full max-w-md max-h-full">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow">
+               <button
+                  class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                  data-modal-hide="add-location-modal">
+                  <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                     xmlns="http://www.w3.org/2000/svg">
+                     <path fill-rule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clip-rule="evenodd"></path>
+                  </svg>
+                  <span class="sr-only">Fermer le modal</span>
+               </button>
+               <div class="px-6 py-6 lg:px-8">
+                  <h3 class="mb-4 text-xl font-medium text-gray-900">Ajouter un lieu</h3>
+                  <form method="post" class="space-y-6" action="locations.php">
+                     <div>
+                        <label for="street" class="block mb-2 text-sm font-medium text-gray-900">Rue*</label>
+                        <input type="text" name="street" id="street"
+                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                           placeholder="Rue Fond des Tawes 251" required>
+                     </div>
+                     <div class="flex space-x-8">
+                        <div>
+                           <label for="city" class="block mb-2 text-sm font-medium text-gray-900">Ville*</label>
+                           <input type="text" name="city" id="city" placeholder="Liège"
+                              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                              required>
+                        </div>
+                        <div>
+                           <label for="city" class="block mb-2 text-sm font-medium text-gray-900">Code
+                              postal* (4 chars max)</label>
+                           <input type="text" name="zip" id="zip" placeholder="4000"
+                              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                              required maxlength="4">
+                        </div>
+                     </div>
+                     <div>
+                        <label for="country" class="block mb-2 text-sm font-medium text-gray-900">Pays*</label>
+                        <input type="text" name="country" id="country"
+                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                           placeholder="Belgique" required>
+                     </div>
+                     <div>
+
+                        <label for="comment" class="block mb-2 text-sm font-medium text-gray-900">Commentaire</label>
+                        <textarea id="comment" rows="4"
+                           class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                           placeholder="Ajouter un commentaire"></textarea>
+                     </div>
+                     <div>
+                        <label for="id" class="block mb-2 text-sm font-medium text-gray-900">ID</label>
+                        <input type="number" name="id" id="id"
+                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                           placeholder="20">
+                     </div>
+                     <button type="submit"
+                        class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Ajouter</button>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+
+
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 </body>
