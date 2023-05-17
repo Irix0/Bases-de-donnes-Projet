543f74743ebb64bd0861f68c212a178d6ef6d770
Roxane Nashroudi <roxanenashroudi@MacBook-Air-de-Roxane.local>
2023-05-17 14:12:45 +0200

    Point 6 start and little changes for edit-song

--- www/dashboard/edit-song.php
+++ www/dashboard/edit-song.php
@@ -106,8 +106,8 @@ require_once(__ROOT__.'/head.php');
          </div>
          <div>
             <input type="hidden" name="action" value="edit">
-            <button type="submit"
-               class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Editer</button>
+                <button type="submit"
+                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Editer</button>
          </div>
       </form>
    </div>
