2446bc851113c54a3a0a1ee5c38bd96805083055
Robert Louan <1413-Louan.Robert@users.noreply.gitlab.uliege.be>
2023-05-18 17:15:19 +0200

    added logo

--- www/navbar.php
+++ www/navbar.php
@@ -4,7 +4,7 @@
       <div class="flex flex-row items-center justify-between lg:justify-start">
          <a class="text-lg tracking-tight text-black uppercase focus:outline-none focus:ring lg:text-2xl" href="/">
             <span class="lg:text-lg focus:ring-0">
-               WeND(Y)
+               <img class="h-16" src="/wendy_logo.png" alt="logo">
             </span>
          </a>
          <button @click="open = !open"
