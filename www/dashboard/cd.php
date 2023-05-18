af46506c1f2ab9c75b0246f322cebc7778f2c5fd
Robert Louan <1413-Louan.Robert@users.noreply.gitlab.uliege.be>
2023-05-18 11:35:13 +0200

    added titles

--- www/dashboard/cd.php
+++ www/dashboard/cd.php
@@ -68,7 +68,7 @@ $resultsPerPage = 10;
                      <div class='flex'>
                      <a href='edit-cd.php?cd_number=" . $row['CD_NUMBER'] . "'>
                         <button data-modal-target='add-cd-modal' data-modal-toggle='add-cd-modal'
-                           class='faa-parent animated-hover text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-3 py-2 text-center mr-2 mb-2'
+                           class='faa-parent animated-hover text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-3 py-2 text-center mt-2 mr-2 mb-2'
                            type='button'>
                            <i class='fa-solid fa-arrow-right faa-bounce'></i>
                         </button>
